ace6d7032873eb5f87772bf78a485a3c129d8bc8
Vishwajeet Singh <vishwajeet.singh.me23@heritageit.edu.in>
2022-01-19 13:00:08 +0530

    Diff modified

--- diff.java
+++ diff.java
@@ -1,7 +1,7 @@
 public class diff {
     public static void main(String[] args) {
-        int a=8;
+        
         int b=4;
-        System.out.println(a-b);
+        System.out.println(b);
     }
 }
